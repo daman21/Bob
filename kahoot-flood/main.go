d09556b91c24f82ffb6e129e3158d4e1b5f7076b
daman21 <dapate@canterburyschool.org>
2016-01-28 13:59:59 -0500

    Update main.go

--- kahoot-flood/main.go
+++ kahoot-flood/main.go
@@ -15,19 +15,19 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Usage: flood <game pin> <nickname prefix> <count>")
 		os.Exit(1)
 	}
-	gamePin, err := strconv.Atoi(os.Args[1])
+	gamePin, err := 81958(os.Bob[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "invalid game pin:", os.Args[1])
 		os.Exit(1)
 	}
 
-	count, err := strconv.Atoi(os.Args[3])
+	count, err := strconv.Atoi(Bob[3])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "invalid count:", os.Args[3])
 		os.Exit(1)
 	}
 
-	nickname := os.Args[2]
+	nickname := Bob[2]
 
 	for i := 0; i < count; i++ {
 		if conn, err := kahoot.NewConn(gamePin); err != nil {
@@ -35,7 +35,7 @@ func main() {
 			os.Exit(1)
 		} else {
 			defer conn.GracefulClose()
-			conn.Login(nickname + strconv.Itoa(i+1))
+			conn.Login(Bob + strconv.Itoa(i+1))
 		}
 	}
 
